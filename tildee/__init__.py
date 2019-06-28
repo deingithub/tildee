f35021b9abec6d5347e12772e2347e556272b760
deing <admin@15318.de>
2019-06-28 19:55:01 +0200

    Cleanup

--- tildee/__init__.py
+++ tildee/__init__.py
@@ -5,13 +5,12 @@ from lxml import html
 
 
 class TildesClient:
-    """A client for the Tildes API."""
+    """A client for the (unstable) Tildes API."""
 
     def __init__(
         self, username, password, base_url="https://tildes.net", verify_ssl=True
     ):
         """Logs in using username and password.
-
         Override base_url and if necessary verify_ssl to change the site Tildee uses."""
         self.username = username
         self.base_url = base_url
@@ -22,8 +21,13 @@ class TildesClient:
         self._verify_ssl = verify_ssl
         self._login(password)
 
+    def __del__(self):
+        self._logout()
+
     def _login(self, password):
-        login_page = requests.get(self.base_url + "/login", verify=self._verify_ssl)
+        login_page = requests.get(
+            self.base_url + "/login", headers=self._headers, verify=self._verify_ssl
+        )
         self._csrf_token = (
             html.fromstring(login_page.content)
             .cssselect("meta[name=csrftoken]")[0]
@@ -43,8 +47,10 @@ class TildesClient:
         login_request.raise_for_status()
         self._cookies = login_page.cookies
 
+    def _logout(self):
+        self._post("/logout")
+
     def _get(self, route):
-        """Fetch a page using HTTP GET. Intended for internal use."""
         r = requests.get(
             self.base_url + route,
             cookies=self._cookies,
@@ -55,7 +61,6 @@ class TildesClient:
         return r
 
     def _post(self, route, **kwargs):
-        """Make a request using HTTP POST. Intended for internal use."""
         r = requests.post(
             self.base_url + route,
             cookies=self._cookies,
@@ -80,7 +85,6 @@ class TildesClient:
 
     def create_topic(self, group, title, tags, **kwargs):
         """Post a topic into a group (without ~).
-        Keyword arguments: `link` and/or `markdown`.
         Returns new topic's id36."""
         r = self._post(f"/~{group}/topics", title=title, tags=tags, **kwargs)
         return r.url.split("/")[-2]
